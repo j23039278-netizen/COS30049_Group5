3ffb6c2957a76e5b6ac4f54c0e0132ba77f6cb2e
Waymiin <swm20040201@gnail.com>
2026-05-28 15:10:49 +0800

    Make phone can view

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,13 @@ app = FastAPI(
 # Allow React frontend to communicate with backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://192.168.100.18:5173",
+        "http://192.168.100.52:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

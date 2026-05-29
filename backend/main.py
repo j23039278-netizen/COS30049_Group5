9410a1c69d499fc4f6f355b7a9ace6eb04f1397a
Waymiin <swm20040201@gnail.com>
2026-05-29 17:06:56 +0800

    Fix error

--- backend/main.py
+++ backend/main.py
@@ -23,10 +23,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://localhost:5174",
         "http://localhost:3000",
         "http://192.168.100.18:5173",
+        "http://192.168.100.18:5174",
         "http://192.168.100.52:5173",
         "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
     ],
     allow_credentials=True,
     allow_methods=["*"],
